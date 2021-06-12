19f4d4768ffaaed97dcdc612e12f716b1511181a
Roothery <roothery@gmail.com>
2021-06-12 11:35:54 -0400

    Criado método Delete

--- src/Api.Application/Controllers/UsersController.cs
+++ src/Api.Application/Controllers/UsersController.cs
@@ -105,5 +105,23 @@ namespace Api.Application.Controllers
                 return StatusCode((int)HttpStatusCode.InternalServerError, ex.Message);
             }
         }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(Guid id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                return Ok(await _service.Delete(id));
+            }
+            catch (ArgumentException ex)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError, ex.Message);
+            }
+        }
     }
 }
